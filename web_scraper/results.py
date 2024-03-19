f9f05f3fddd5eeec39dbb28d8face1ffc8704dc5
CHEJARLA BALAJI <143786112+Chejarlabalaji2112@users.noreply.github.com>
2024-03-19 06:46:38 +0530

    Add files via upload

--- web_scraper/results.py
+++ web_scraper/results.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def checker(results_type, available_results_links):  # results_type specifies what results we are looking for and available_results_links are to check whether the results we are looking is there or not.
+    """This function checks whether the specified year results released or not. """
+    pass
+
+
+def html_res_par(url):
+    """This function gets response and parse the response"""
+    response = urlopen(url).read()
+    soup = BeautifulSoup(response, "html.parser")
+    all_available_links = soup.find
+
+
+
 
